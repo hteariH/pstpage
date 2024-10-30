1027388385a7ebe397bace00d1e585667c2a4217
oberkunskyi <oleksii.berkunskyi@ocado.com>
2024-10-30 15:31:36 +0000

    Add deployment script and update GitHub Actions workflow
    Introduce `deploy.sh` to automate stopping, updating, and starting the Java application. Modify the GitHub Actions workflow to use this deployment script instead of directly running the Spring Boot application.

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Find and stop the running Java application
+pkill -f pstpage.jar
+
+# Copy the new JAR file to the desired location
+cp ./target/pstpage-0.0.1-SNAPSHOT.jar /home/admin/deploy/pstpage.jar
+
+#Start the Java application using the new JAR
+nohup java -jar /home/admin/deploy/pstpage.jar > /dev/null 2>&1 &
