84993794400308dacc259e5a62af0c1df70d7efc
oberkunskyi <oleksii.berkunskyi@ocado.com>
2024-10-30 14:27:13 +0000

    Enable custom servlet container, disable HTTP to HTTPS redirect
    Re-enable `ServletContainerCustomizer` to customize Tomcat connector and set the port to 8080. Temporarily disable `RedirectHttpToHttpsConfig` to stop HTTP to HTTPS redirection configuration.

--- src/main/java/com/mamoru/pstpage/RedirectHttpToHttpsConfig.java
+++ src/main/java/com/mamoru/pstpage/RedirectHttpToHttpsConfig.java
@@ -9,35 +9,35 @@ import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-public class RedirectHttpToHttpsConfig {
-    @Bean
-    public ServletWebServerFactory servletContainer() {
-
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
-            @Override
-            protected void postProcessContext(Context context) {
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-
-                SecurityCollection collection = new SecurityCollection();
-                collection.addPattern("/*");
-
-                securityConstraint.addCollection(collection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-
-        tomcat.addAdditionalTomcatConnectors(redirectConnector());
-        return tomcat;
-    }
-
-    private Connector redirectConnector() {
-        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-        connector.setScheme("http");
-        connector.setPort(8080);
-        connector.setSecure(false);
-        connector.setRedirectPort(8443);
-        return connector;
-    }
-}
+//@Configuration
+//public class RedirectHttpToHttpsConfig {
+//    @Bean
+//    public ServletWebServerFactory servletContainer() {
+//
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory() {
+//            @Override
+//            protected void postProcessContext(Context context) {
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//
+//                SecurityCollection collection = new SecurityCollection();
+//                collection.addPattern("/*");
+//
+//                securityConstraint.addCollection(collection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//
+//        tomcat.addAdditionalTomcatConnectors(redirectConnector());
+//        return tomcat;
+//    }
+//
+//    private Connector redirectConnector() {
+//        Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+//        connector.setScheme("http");
+//        connector.setPort(8080);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(8443);
+//        return connector;
+//    }
+//}
--- src/main/java/com/mamoru/pstpage/ServletContainerCustomizer.java
+++ src/main/java/com/mamoru/pstpage/ServletContainerCustomizer.java
@@ -5,15 +5,15 @@ import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactor
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.stereotype.Component;
 
-//@Component
-//public class ServletContainerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
-//
-//    @Override
-//    public void customize(TomcatServletWebServerFactory factory) {
-//
-//       Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
-//       connector.setPort(8080);
-//
-//       factory.addAdditionalTomcatConnectors(connector);
-//    }
-//}
+@Component
+public class ServletContainerCustomizer implements WebServerFactoryCustomizer<TomcatServletWebServerFactory> {
+
+    @Override
+    public void customize(TomcatServletWebServerFactory factory) {
+
+       Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
+       connector.setPort(8080);
+
+       factory.addAdditionalTomcatConnectors(connector);
+    }
+}
